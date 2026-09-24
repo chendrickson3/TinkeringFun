e2f354cc42fd466d9b2cac1b9bb9602ded9cbd65
Caden Hendrickson <chendrickson3@zagmail.gonzaga.edu>
2026-09-24 11:10:20 -0700

    Example4_functions_lab

--- variableslab.py
+++ variableslab.py
@@ -10,3 +10,17 @@ print("power:",power)
 print(type(power))
 
 # it's tuesday!!
+
+
+def calculate_jump_distance(speed, airtime):
+
+def main():
+    user_speed = float(input("enter horizontal speed in m/s: "))
+    user_airtime = float(input("enter airtime in seconds: "))
+    jump_distance = calculate_jump_distance(user_speed, user_airtime)
+
+    print(f"speed: {user_speed} m/s ") 
+    print(f"airtime: {user_airtime} seconds")
+    print(f"jump distance: {round(jump_distance, 2)} meters")
+
+main()
